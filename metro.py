a30736ba9674624ac61950b4bf1a91bd70a293b0
frs84 <49400641+frs84@users.noreply.github.com>
2025-06-19 10:17:39 +0300

    Update metro.py

--- metro.py
+++ metro.py
@@ -8,7 +8,7 @@ sns.set_theme(style="darkgrid")  # active le style seaborn
 
 st.title("Moscow Metro Ticket Selector")
 
-trips_per_week_input = st.number_input("Number of trips per week", min_value=1, max_value=15, value=10)
+
 
 # Colonnes : inputs à gauche, graphique à droite
 col1, col2 = st.columns([1, 2])
@@ -19,15 +19,19 @@ with col1:
                     index=0,
                     horizontal=True)
 with col2:
-    payment = st. radio("Select mean of payment:",
-                       ["Troïka", "Bank card"],
-                       index=0,
-                       horizontal=True)
+    trips_per_week_input = st.number_input("Number of trips per week", min_value=1, max_value=15, value=10)
 
-with col1:
-    onetrip = st.number_input("Price for 1 trip (₽)", value=67)
-    onemonth = st.number_input("Price for 1-month pass (₽)", value=3160)
-    threemonths = st.number_input("Price for 3-month pass (₽)", value=7650)
+choice = [67,3160,7650] if zone == "Moscow" else [90,3940,10080]
+onetrip, onemonth, threemonths = choice
+
+st.write(f"""
+    - one trip = {onetrip}r.,\n
+    - one month pass = {onemonth}r.,\n
+    - three months = {threemonths}r.
+    """)
+
+priceMsc = {onetrip : 67, onemonth: 3160, threemonths : 7650}
+priceMscMO = {onetrip : 90, onemonth: 3940, threemonths : 10080}
 
 # Ici on considère que "trips" est le nombre de trajets par semaine
 trips = np.arange(5, 16)  # par exemple de 5 à 27 trajets par semaine
@@ -42,16 +46,15 @@ df = pd.DataFrame({
 # Format "long" pour seaborn
 df_long = df.melt(id_vars="Trips per week", var_name="Ticket Type", value_name="Cost per Trip (₽)")
 
-with col2:
-    plt.figure(figsize=(10, 6))
-    sns.lineplot(data=df_long, x="Trips per week", y="Cost per Trip (₽)", hue="Ticket Type", marker="o")
-    plt.axvline(x=trips_per_week_input, color='red', linestyle='--', linewidth=2)
-    plt.title("Cost per Trip vs Number of Weekly Trips", fontsize=16)
-    plt.xlabel("Number of Trips per Week")
-    plt.ylabel("Cost per Trip (₽)")
-    plt.grid(True)
-    plt.legend(title="Ticket Type")
-    st.pyplot(plt.gcf())
+plt.figure(figsize=(10, 6))
+sns.lineplot(data=df_long, x="Trips per week", y="Cost per Trip (₽)", hue="Ticket Type", marker="o")
+plt.axvline(x=trips_per_week_input, color='red', linestyle='--', linewidth=2)
+plt.title("Cost per Trip vs Number of Weekly Trips", fontsize=16)
+plt.xlabel("Number of Trips per Week")
+plt.ylabel("Cost per Trip (₽)")
+plt.grid(True)
+plt.legend(title="Ticket Type")
+st.pyplot(plt.gcf())
 
 st.write(f"""For {trips_per_week_input} trips per week, the cost is:
 - {onetrip*trips_per_week_input}r. with one trip payments ({74*trips_per_week_input}r. if you pay per bank card),
